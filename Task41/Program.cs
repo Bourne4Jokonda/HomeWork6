a9a7d063ad9650373e68bf0086d0ac24ab8a1749
«Aleksandr» <bourne4jokonda@gmail.com>
2022-07-03 15:24:49 +0300

    task 41 complete

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+int[]numbers=new int[5];
+int sum=0;
+for(int i=0;i<numbers.Length;i++)
+{
+      Console.Write($"Введите число {i+1}: ");
+      numbers[i]=Convert.ToInt32(Console.ReadLine());
+      if(numbers[i]>=1)
+      {
+            sum++;
+      }
+}
+Console.WriteLine("Количество чисел больше 0 : "+sum);
